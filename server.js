358be3326b0a56916447658e419a342b2b86dee5
Javan <71285290+djavanw@users.noreply.github.com>
2021-04-24 08:56:16 -0600

    Deploying Application.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const fs = require("fs");
-const util = require("util");
-const path = require("path");
+//const fs = require("fs");
+//const util = require("util");
+//const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3769;
 
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
+//Routes
 require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
 
